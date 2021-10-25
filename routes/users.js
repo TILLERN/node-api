37b0e3db6a237e61fae8972fa7b3f3d644601930
TILLERN <brysonminodi@google.com>
2021-10-25 16:20:20 +0300

    controller seperation

--- routes/users.js
+++ routes/users.js
@@ -1,53 +1,17 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import { getUser, getUsers, createUser, updateUser, deleteUser } from '../controllers/users.js'
 
 const router = express.Router();
 
-const users = [
-  
-]
+router.get('/', getUsers);
 
-router.get('/', (req, res) => {
-  res.send(users);
+router.get('/:id', getUser);
 
-});
+router.post('/', createUser);
 
-router.get('/:id', (req, res)=>{
-   const { id } = req.params;
-   const foundUser = users.find( (user) => user.id == id);
+router.delete('/:id', deleteUser);
 
-   res.send(users);
-});
-
-router.post('/', (req, res)=>{
-    const user = req.body;
-
-    users.push( { ...user, id:uuidv4()});
-
-    res.send(`${user.firstName} added to the database!`);
-});
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    users = users.filter((user) => user.id !== id); 
-
-    req.send(`user withn the id ${id} deleted from the database`);
-});
-
-
-router.patch('/:id', (req, res)=> {
-  const { id } = req.params;
-  const {firstName, lastName, age} = req.body;
-
-  const user = users.find((user) => user.id == id);
-
-  if (firstName) user.firstName = firstName;
-  if (lastName) user.lastName = lastName;
-  if (age) user.age = age;
-
-  res.send(`user with the id ${id} has been updated`);
-
-});
+router.patch('/:id', updateUser);
 
 export default router;
